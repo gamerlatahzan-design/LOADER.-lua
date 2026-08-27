1850e371ceb7eaf5c3b21e7b8674ccc2bfaed438
gamerlatahzan-design <gamerlatahzan@gmail.com>
2026-08-28 00:01:53 +0700

    Update loader free version v2.lua

--- loader free version v2.lua
+++ loader free version v2.lua
@@ -66,24 +66,6 @@ local SupportedGames = {
                 ScriptURL = "SourceCodeMuscleLegends.lua"
             }
         }
-    },
-    [3260590327] = {
-        GameName = "Tower Defense Simulator",
-        Options = {
-            {
-                Name = "Tower Defense Simulator",
-                ScriptURL = "SourceCodeTowerDefenseSimulator.lua"
-            }
-        }
-    },
-    [5591597781] = {
-        GameName = "Tower Defense Simulator",
-        Options = {
-            {
-                Name = "Tower Defense Simulator",
-                ScriptURL = "SourceCodeTowerDefenseSimulator.lua"
-            }
-        }
     }
 }
 
